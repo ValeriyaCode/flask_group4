2dccfa929dc3d226f9b98b8d1ec5eaa17c1a31a5
LeraTest <lera@example.com>
2026-06-25 21:10:10 +0300

    Train hello

--- app.py
+++ app.py
@@ -1,5 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, flash, url_for
 
 app = Flask(__name__)
+app.secret_key = '***' # потрібен для flash-повідомлення
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        name = request.form.get('name')
+
+        # flash - функція, для відправки повідомлення в межах наступного запиту
+        flash(f'Hello, {name}!')
+
+        # redirect - функція, яка перенаправляє на потрібну сторінку з методом GET
+        # url_for - дозволяє посилатись на функцію, яка обробляє певний маршрут
+        return redirect(url_for('index'))
+
+    return render_template('index.html')
+
 
 app.run(debug=True)
